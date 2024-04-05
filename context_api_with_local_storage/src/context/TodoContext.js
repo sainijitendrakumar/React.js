a37e6d206e6c7e7f33c91f6b2244180455e373b8
Jitendra Kumar Saini <65939231+sainijitendrakumar@users.noreply.github.com>
2024-04-05 10:49:38 +0530

    Add files via upload

---
+++ context_api_with_local_storage/src/context/TodoContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [{
+        id: 1,
+        todo: "todo msg",
+        completed:false
+    }],
+    addTodo:(todo)=>{},
+    updatedTodo:(id,todo)=>{},
+    deletedTodo:(id)=>{},
+    toggleCompleted:(id)=>{}
+})
+
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
